91127ac2154e4cfb5442a42611b56f365ea4e7ef
Billy Gordon <Billywgordon93@gmail.com>
2020-10-27 14:13:21 -0500

    CreatedExercise

--- Program.cs
+++ Program.cs
@@ -5,8 +5,27 @@ namespace SwitchStatementExercize
     class Program
     {
         static void Main(string[] args)
+            
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi What is your favorite subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Do you like math? I always found numbers annoying!");
+                    break;
+                case "Science":
+                    Console.WriteLine("No Way!!! Science is my favorite also!");
+                    break;
+                case "lunch":
+                    Console.WriteLine("The only class I ever passed");
+                    break;
+                default:
+                    Console.WriteLine("wierd I have never heard of that class before?");
+                    break;
+            }
         }
     }
 }
